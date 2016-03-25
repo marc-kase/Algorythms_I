fbbbdcdda6163d04c316fa6f1e2b5971f9491a80
markgereski <mark.gereski@gmail.com>
2016-03-25 14:35:49 +0600

    Puzzle: evaluating the time

--- Puzzle/src/Client.java
+++ Puzzle/src/Client.java
@@ -5,8 +5,12 @@ import java.nio.file.Paths;
 
 public class Client {
     public static void main(String[] args) {
-//        String filename = "puzzle3x3-08.txt";
-        String filename = "puzzle3x3-25.txt";
+        java.util.Date d = new java.util.Date();
+        StdOut.println("Started at " + d);
+        long t0 = System.currentTimeMillis();
+
+        String filename = "puzzle3x3-08.txt";
+//        String filename = "puzzle3x3-25.txt";
 //        String filename = "puzzle4x4-31.txt";
 //        String filename = "puzzle04.txt";
         Path f = Paths.get(args[0], filename);
@@ -32,5 +36,12 @@ public class Client {
             for (Board board : solver.solution())
                 StdOut.println(board.toString());
         }
+
+        long t1 = System.currentTimeMillis();
+
+        StdOut.println("------------------------------------");
+        StdOut.println("Started at: " + d);
+        StdOut.println("Stoped  at: " + new java.util.Date());
+        StdOut.println("Evaluated time: " + (t1 - t0) + " msec");
     }
 }
